f209740136708ef4c1c2bfdfbf9e0e368b44d088
Damon <dbolesta@users.noreply.github.com>
2022-10-08 01:00:57 -0400

    adds simple parallax scolling components via react-scroll-parallax

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Page from './components/Page';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 
 function App() {
   return (
     <div className="App">
-      <Page />
-      <Page />
-      <Page />
+      <ParallaxProvider>
+        <Page start='-200px' end='500px' />
+        <Page start='-50px' end='200px' />
+        <Page start='-200px' end='400px' />
+      </ParallaxProvider>
     </div>
   );
 }
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,18 +1,25 @@
 import styled from 'styled-components';
+import { Parallax } from 'react-scroll-parallax';
 
 const StyledPage = styled.div`
     border: 1px solid red;
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 100vw;
+    width: 100%;
     height: 100vh;
 `;
 
 
-const Page = () => {
+const Page = ({start, end}) => {
   return (
-    <StyledPage>Page</StyledPage>
+    <StyledPage>
+        {/* <Parallax translateY={['-100px', '200px']}> */}
+        <Parallax translateY={[start, end]} >
+        {/* <Parallax speed={-15}> */}
+            <div className="my-thing" />
+        </Parallax>
+    </StyledPage>
   )
 }
 
